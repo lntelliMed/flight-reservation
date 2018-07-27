3870b8ff601f2254a356b860c94ca08ad58c5e89
“Sam <sam.alsmadi@gmail.com>
2018-07-26 23:20:57 -0400

    Add Logging to controller layer

--- src/main/java/com/lntellimed/flightreservation/controllers/ReservationController.java
+++ src/main/java/com/lntellimed/flightreservation/controllers/ReservationController.java
@@ -1,5 +1,7 @@
 package com.lntellimed.flightreservation.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -22,15 +24,20 @@ public class ReservationController {
 	@Autowired
 	ReservationService reservationService;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReservationController.class);
+
 	@RequestMapping("showCompleteResvervation")
 	public String showCompleteResvervation(@RequestParam("flightId") Long flightId, ModelMap modelMap) {
+		LOGGER.info("Inside showCompleteResvervation() invoked with the Flight ID: " + flightId);
 		Flight flight = flightRepository.findById(flightId).get();
 		modelMap.addAttribute("flight", flight);
+		LOGGER.info("Flight is: " + flight);
 		return "completeReservation";
 	}
 	
 	@RequestMapping(value="/completeReservation", method=RequestMethod.POST)
 	public String completeReservation(ReservationRequest request, ModelMap modelMap) {
+		LOGGER.info("Inside completeReservation() " + request);
 		Reservation reservation = reservationService.bookFlight(request);
 		modelMap.addAttribute("msg", "Reservation created successfully and the id is " + reservation.getId());
 		return "reservationConfirmation";
--- src/main/java/com/lntellimed/flightreservation/controllers/UserController.java
+++ src/main/java/com/lntellimed/flightreservation/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.lntellimed.flightreservation.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,30 +13,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.lntellimed.flightreservation.entities.User;
 import com.lntellimed.flightreservation.repos.UserRepository;
 
+
 @Controller
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+	
 	@RequestMapping("/showReg")
 	public String showRegistrationPage() {
+		LOGGER.info("Inside showRegistrationPage()");
 		return "login/registerUser";
 	}
 	
 	@RequestMapping(value="/registerUser", method=RequestMethod.POST)
 	public String register(@ModelAttribute("user") User user) {
+		LOGGER.info("Inside register() " + user);
 		userRepository.save(user);
 		return "login/login";
 	}
 	
 	@RequestMapping("/showLogin")
 	public String showLoginPage() {
+		LOGGER.info("Inside showLoginPage()");
 		return "login/login";
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(@RequestParam("email") String email, @RequestParam("password") String password, ModelMap modelMap) {
+		LOGGER.info("Inside login() and the email is: " + email);
 		User user = userRepository.findByEmail(email);
 		if (user.getPassword().equals(password)) {
 			return "findFlights";
