20bac433fd505720108f588c0ad1d9778a004807
“Sam <sam.alsmadi@gmail.com>
2018-07-27 18:58:42 -0400

    Add AuthenticationManager bean

--- src/main/java/com/lntellimed/flightreservation/security/config/WebSecurityConfig.java
+++ src/main/java/com/lntellimed/flightreservation/security/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.lntellimed.flightreservation.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +22,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/showReg","/","/index.html","/registerUser","/login","/showLogin","/login/*").permitAll()
 			.antMatchers("/admin/showAddFlight").hasAnyAuthority("ADMIN").anyRequest().authenticated().and().csrf().disable();
+	}
+	
+	@Bean(name=BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
